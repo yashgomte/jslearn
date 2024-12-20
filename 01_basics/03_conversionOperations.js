781241421cd84cc017967070df4be8979c1ecd9f
yashgomte <153908502+yashgomte@users.noreply.github.com>
2024-12-20 06:13:16 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -19,7 +19,42 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 27
  let stringNumber = String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+ //console.log(stringNumber);
+ //console.log(typeof stringNumber);
+ 
+ // ******************************** Operations *************************
+ 
+ let value = 3
+ let negValue = -value
+ // console.log(negValue);
+
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2/2);
+ //console.log(2**3);
+ //console.log(2%3);
+
+ let str1 = "hello"
+ let str2 = " Yash"
+
+ let str3 = str1 + str2
+ // console.log(str3);
+
+//  console.log("1"+ 2);
+//  console.log(1+ "2");
+//  console.log("1"+ 2 + 2);
+//  console.log(1+ 2 + "2");
+
+//  console.log(+true);
+//  console.log(+"");
+
+ let num1, num2, num3
+ num1 = num2 = num3 = 2 + 2
+
+ let gamecounter = 100
+ ++gamecounter;
+ console.log(gamecounter);
+ 
  
  
