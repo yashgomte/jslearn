e06a4d9e245caf18d61d7d87242342579016710c
yashgomte <153908502+yashgomte@users.noreply.github.com>
2024-12-20 05:43:27 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "35ads"
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "35" => 33
+// "3abc"=> NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 =>true; 0=> false 
+// "" => false
+// "yash"=> true
+
+let someNumber = 27
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+ 
