3c56924989813dfb1aa3b89966b4d1d7101b29de
yashgomte <153908502+yashgomte@users.noreply.github.com>
2024-12-20 06:57:59 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+
+// avoiding this comparision
+console.log(null > 0);
+console.log(null == 0); // comparision and equality check are work differently
+console.log(null >= 0);
+
+console.log(undefined == 0); // false
+console.log(undefined >= 0);
+console.log(undefined < 0);
+
+// ===  => it checks stricktly and check data tpe also 
+
+console.log("2"=== 2); // false
